adabae7b96b56458142d0feed46cad01a2cedf1d
Kameron Watts <kam.watts@gmail.com>
2023-01-25 17:08:56 -0800

    Updated functionality and added a HTML list

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# about-me-kam
+# LAB - 02
+
+## About Me Project
+
+I constructed an app using HTML, CSS, and JS. Upon entering the page you will be presented with a set of questions, after the questions are answered, you enter the website.
+
+I am implementing an interactive questionnaire that utilizes JavaScript to iterate through wrong and right answers as the user guesses questions about my interests.
+
+### Author: Kameron D. Watts
+
+### Links and Resources
+
+- [Geeks for Geeks]("https://www.geeksforgeeks.org/css-combine-background-image-with-gradient-overlay/")
+- [Meyer Web]("https://meyerweb.com/eric/tools/css/reset/")
+- []
+
+### Lighthouse Accessibility Report Score
+
+[Day 1 Accessibility Report](img/Screenshot%202023-01-24%20at%203.48.48%20PM.png)
+
+### Reflections and Comments
+
+- Consider including the answers to your daily journal and submission questions here
+- This is also a good place to reflect on the tools and resources used and learned
--- js/app.js
+++ js/app.js
@@ -13,13 +13,14 @@ One true and it will run
 */
 
 // Question about the users name stored with a variable and prints a message/alert after the users input
-
+// QUESTION 1
 let yourName = prompt('What is your name?');
 console.log('Hello " + yourName + " welcome to my portfolio!');
 alert('Hello ' + yourName + ', welcome to my portfolio!');
 
 // Question asks the user if they are a recruiter or business owner looking for employees
 
+// QUESTION 2
 let yourOccupation = prompt('Are you looking to hire?').toLowerCase();
 
 if (yourOccupation === 'yes' || yourOccupation === 'y') {
@@ -34,7 +35,7 @@ if (yourOccupation === 'yes' || yourOccupation === 'y') {
 }
 
 // Question asks if the user specifically wants a JavaScript Developer
-
+// QUESTION 3
 let whatsYourStack = prompt('Are you looking for a JavaScript Developer?').toLowerCase();
 
 if (whatsYourStack === 'yes' || whatsYourStack === 'y') {
@@ -49,7 +50,7 @@ if (whatsYourStack === 'yes' || whatsYourStack === 'y') {
 }
 
 // Question asks if the user values a growth mindset
-
+// QUESTION 4
 let yourMindset = prompt('Do you value a growth mindset?').toLowerCase();
 
 if (yourMindset === 'yes' || yourMindset === 'y') {
@@ -64,7 +65,7 @@ if (yourMindset === 'yes' || yourMindset === 'y') {
 }
 
 // Question asks if the user like Code Fellows program graduates
-
+// QUESTION 5
 let graduates = prompt('Do you like software engineer graduates from Code Fellows?').toLowerCase();
 
 if (graduates === 'yes' || graduates === 'y') {
@@ -78,3 +79,71 @@ if (graduates === 'yes' || graduates === 'y') {
   alert('Please answer with a yes or no');
 }
 
+// QUESTION 6
+/*
+
+Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+Indicates through an alert if the guess is “too high” or “too low”.
+It should give the user exactly four opportunities to get the correct answer.
+After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+
+user is prompted to guess
+
+compare that variable
+
+- too high
+- too low
+- corrrect answer
+- not a num
+- not a valid number in the range
+- if out of guesses, give the answer
+
+*/
+
+let maxAttempts = 3;
+let score = 0;
+
+for (let i = 0; i <= maxAttempts; i++) {
+
+  console.log(maxAttempts);
+  console.log(i);
+
+  let myFavMeal = prompt('How many meals do I eat in one day?').toLowerCase();
+
+  if (myFavMeal === 'three' || myFavMeal === '3') {
+    score++;
+    console.log(`You are correct ${yourName} and here is your score: ${score}` );
+    alert(`You are correct ${yourName} and here is your score: ${score}`);
+    break;
+  } else {
+    console.log(`Try again, you have ${maxAttempts - i} tries remaining!`);
+    alert(`Try again, you have ${maxAttempts - i} tries remaining!`);
+  }
+}
+
+// QUESTION 7
+
+/*
+
+Add a 7th question that has multiple possible correct answers that are stored in an array.
+Give the user 6 attempts to guess the correct answer.
+The guesses will end once the user guesses a correct answer or they run out of attempts.
+Display all the possible correct answers to the user.
+Consider using a loop of some sort for this question.
+
+*/
+
+let howManyDays = prompt('How many days does it take to drive to Seattle from Alaska?').toLowerCase();
+let correctAnswers = [1, 2, 3];
+let mostAttempts = 5;
+
+for (let i = 0; i <= mostAttempts; i++) {
+
+  if (howManyDays === correctAnswers.indexOf()) {
+    console.log(`Good job ${yourName} you are on fire!`);
+    alert(`Good job ${yourName} you are on fire!`);
+  } else {
+    console.log(`Oh no, ${yourName}, you have to try again with ${mostAttempts - i} left.`);
+    alert(`Oh no, ${yourName}, you have to try again with ${mostAttempts - i} tries left.`);
+  }
+}
